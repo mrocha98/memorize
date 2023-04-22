12bb08bdb11977a2d1db03d712c616b5c7130649
Matheus Rocha <matheus_rocha98@outlook.com>
2023-04-21 22:29:15 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Matheus Rocha on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
