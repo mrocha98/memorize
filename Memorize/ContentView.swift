463890d65076fd6807b8c3532cee042267fc11f6
Matheus Rocha <matheus_rocha98@outlook.com>
2023-04-24 16:18:08 -0300

    finish lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  Memorize
-//
-//  Created by Matheus Rocha on 21/04/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            ForEach(0..<4) {
+                index in
+                CardView(isFaceUp: index.isMultiple(of: 2))
+            }
         }
-        .padding()
+            .padding()
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill()
+            }
+        }
     }
 }
 
